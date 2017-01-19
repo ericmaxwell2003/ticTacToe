d365f371a70b43c424d31204da8a73ef2b6c6f96
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-01-19 15:52:51 -0800

    Finished working MVC

--- app/src/main/java/com/acme/tictactoe/model/Board.java
+++ app/src/main/java/com/acme/tictactoe/model/Board.java
@@ -22,6 +22,7 @@ public class Board {
      */
     public void restart() {
         clearCells();
+        winner = null;
         currentTurn = Player.X;
         state = GameState.IN_PROGRESS;
     }
@@ -33,11 +34,17 @@ public class Board {
      *
      * @param row 0..2
      * @param col 0..2
+     * @return the player that moved or null if we did not move anything.
      *
      */
-    public void mark( int row, int col ) {
+    public Player mark( int row, int col ) {
+
+        Player playerThatMoved = null;
+
         if(isValid(row, col)) {
+
             cells[row][col].setValue(currentTurn);
+            playerThatMoved = currentTurn;
 
             if(isWinningMoveByPlayer(currentTurn, row, col)) {
                 state = GameState.FINISHED;
@@ -48,6 +55,8 @@ public class Board {
                 flipCurrentTurn();
             }
         }
+
+        return playerThatMoved;
     }
 
     public Player getWinner() {
