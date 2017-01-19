5ed3c8bda3dee9971000398ba3caac34326f52a2
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-01-19 14:38:00 -0800

    View complete

--- app/src/main/java/com/acme/tictactoe/controller/TicTacToeActivity.java
+++ app/src/main/java/com/acme/tictactoe/controller/TicTacToeActivity.java
@@ -2,14 +2,31 @@ package com.acme.tictactoe.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.acme.tictactoe.R;
+import com.acme.tictactoe.model.Board;
 
 public class TicTacToeActivity extends AppCompatActivity {
+
+    private static String TAG = TicTacToeActivity.class.getName();
+
+    private Board gameBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tictactoe);
+        gameBoard = new Board();
+    }
+
+    public void onCellClicked(View button) {
+
+        String tag = button.getTag().toString();
+        int row = Integer.valueOf(tag.substring(0,1));
+        int col = Integer.valueOf(tag.substring(1,2));
+        Log.i(TAG, "Click Row: [" + row + "," + col + "]");
+
     }
 }
